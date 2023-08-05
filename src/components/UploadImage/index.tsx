78f1786e8276a757f240472a8a6d8c5cdf88a6e2
seikou <seikou615@gmail.com>
2023-08-05 13:31:35 +0100

    Add: [UploadImage] component

---
+++ src/components/UploadImage/index.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import FileBase from "react-file-base64"
+import { UploadImageIcon } from "icons"
+import styles from "./UploadImage.module.css"
+import { useState } from "react"
+
+function UploadImage() {
+
+    const [file, setFile] = useState("")
+
+
+  return (
+    <div className={styles.uploadImage}>
+      <FileBase
+        type="file"
+        multiple={false}
+        onDone={({ base64 }: { base64: string }) => setFile(base64)}
+      />
+      <div
+        className={`${styles.uploadImage__image} ${file && styles.uploadImage__image_withImage}`}
+        style={{ backgroundImage: `url(${file})` }}
+      >
+        <div className={styles.uploadImage__container}>
+          <UploadImageIcon />
+          <h2>{file ? "Change Image" : "+ Upload Image"}</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UploadImage
