3e17ea9812aa789c540ed50cf34bfd74482667b2
seikou <seikou615@gmail.com>
2023-08-16 17:14:56 +0100

    Add: custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import { PageNotFound } from '@/components'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <PageNotFound />
+    </div>
+  )
+}
+
+export default NotFound
