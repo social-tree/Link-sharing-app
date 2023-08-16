066c5e9c503457180ac68118286153c31f11cdc8
Whitelistedd <geforcexxyy@gmail.com>
2023-08-17 00:23:26 +0300

    added socialMediaButton. edited Button

--- src/components/shared/Button/index.tsx
+++ src/components/shared/Button/index.tsx
@@ -4,34 +4,15 @@ import { SimpleIcons } from '../SimpleIcons'
 import classNames from 'classnames'
 import styles from './Button.module.scss'
 
-export function Button({
-  children,
-  variant,
-  socialIcon,
-  backgroundColor,
-  ...props
-}: IButtonProps) {
+export function Button({ children, variant, ...props }: IButtonProps) {
   const buttonClasses = classNames([styles.button], {
     [styles['button--primary']]: variant === 'primary',
     [styles['button--secondary']]: variant === 'secondary',
-    [styles['button--socialMedia']]: variant === 'socialMedia',
   })
 
   return (
-    <button
-      style={{ background: backgroundColor }}
-      className={`${buttonClasses}`}
-      {...props}
-    >
-      {variant === 'socialMedia' ? (
-        <>
-          {socialIcon && <SimpleIcons size={20} name={socialIcon} />}
-          {children}
-          <ArrowRightIcon className={styles.button__socialArrow} />
-        </>
-      ) : (
-        children
-      )}
+    <button className={`${buttonClasses}`} {...props}>
+      {children}
     </button>
   )
 }
