db5e744570933f4f239cca3ecf0c0616508bd29c
seikou <seikou615@gmail.com>
2023-08-30 09:25:03 +0100

    Add: `Platforms` component

---
+++ src/components/Platforms/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Platform } from "./Platform"
+import styles from './Platforms.module.scss'
+import { useDataContext } from "@/Contexts"
+
+export function Platforms() {
+
+  const { fields } = useDataContext()
+
+  return (
+    <div className={styles.platforms}>
+      {fields.map((field: any, i: number) => (
+        <Platform {...{ field, i }} />
+      ))}
+    </div>
+  )
+}
