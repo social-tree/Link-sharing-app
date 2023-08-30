49ef1788d503afd92b4731b1b388b62cfbd397f9
seikou <seikou615@gmail.com>
2023-08-30 09:23:08 +0100

    Add: `Links` page

---
+++ src/app/(pages)/profile/links/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Button, NoLinks, Platforms } from 'components'
+
+import styles from './links.module.scss'
+import { useDataContext } from '@/Contexts'
+
+function Links() {
+
+  const { fields, addLink } = useDataContext()
+
+  return (
+    <div className={styles.links}>
+      <h1>Customize your links</h1>
+      <p className={styles.links__description}>
+        Add/edit/remove links below and then share all your profiles with the
+        world!
+      </p>
+      <Button onClick={addLink} className={styles['links__button--add']} variant="secondary">
+        + Add new link
+      </Button>
+      
+      {fields.length == 0 ? ( <NoLinks />) : <Platforms /> }
+    </div>
+  )
+}
+
+export default Links
